9462710ec2bdf7188b1f31ddb9b156002ab45ad2
Will Dey <will_dey@college.harvard.edu>
2023-08-27 10:05:15 -0400

    Implementation

---
+++ fast_heap.hpp
@@ -0,0 +1,16 @@
+#include<queue>
+#include<vector>
+#include<functional>
+
+template<
+    typename T,
+    typename Container = std::vector<T>,
+    typename Compare = std::less<T>
+>
+class mutable_priority_queue : public std::priority_queue<T, Container, Compare> {
+public:
+    void replace_top(const T& new_top) {
+        this->c.push_back(new_top);
+        this->pop();
+    }
+};
